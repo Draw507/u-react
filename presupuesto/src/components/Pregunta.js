02a9f6a30bdb2d2f78b308d0154666dda15433f1
David Robinson <david.robinson@logicstudio.net>
2020-04-26 07:44:13 -0500

    feat(presupuesto-app): useState

--- presupuesto/src/components/Pregunta.js
+++ presupuesto/src/components/Pregunta.js
@@ -1,17 +1,30 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Pregunta = () => {
+    const [cantidad, guardarCantidad] = useState(0);
+
+    const definirPresupuesto = e => {
+        guardarCantidad(parseInt(e.target.value, 10));
+    };
+
+    const agregarPresupuesto = e => {
+        e.preventDefault();
+    };
+
     return ( 
         <Fragment>
             <h2>
                 Coloca tu presupuesto
             </h2>
 
-            <form>
+            <form
+                onSubmit={agregarPresupuesto}
+            >
                 <input 
                     type="number"
                     className="u-full-width"                    
                     placeholder="Coloca tu presupuesto"
+                    onChange={definirPresupuesto}
                 />
 
                 <input 
