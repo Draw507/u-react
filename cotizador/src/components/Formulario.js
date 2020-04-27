f85501bd9d604aca0de523799c4c79fff48aa451
David Robinson <david.robinson@logicstudio.net>
2020-04-27 13:06:37 -0500

    feat(cotizador-app): validaciones del formulario

--- cotizador/src/components/Formulario.js
+++ cotizador/src/components/Formulario.js
@@ -41,6 +41,15 @@ const Boton = styled.button`
     }
 `;
 
+const Error = styled.div`
+    background-color: red;
+    color: white;
+    padding: 1rem;
+    width: 100%;
+    text-align: center;
+    margin-bottom: 2rem;
+`;
+
 const Formulario = () => {
 
     const [datos, guardarDatos] = useState({
@@ -48,6 +57,7 @@ const Formulario = () => {
         year: '',
         plan: ''
     });
+    const [error, guardarError] = useState(false);
 
     const {marca, year, plan} = datos;
 
@@ -58,8 +68,24 @@ const Formulario = () => {
         });
     };
 
+    const cotizarSeguro = e => {
+        e.preventDefault();
+
+        if(marca.trim() === '' || year.trim() === '' || plan.trim() === ''){
+            guardarError(true);
+            return;
+        }
+
+        guardarError(false);
+
+        
+    };
+
     return ( 
-        <form>
+        <form
+            onSubmit={cotizarSeguro}
+        >
+            {error ? <Error>Todos los campos son obligatorios</Error> : null}
             <Campo>
                 <Label>Marca</Label>
                 <Select
@@ -113,7 +139,7 @@ const Formulario = () => {
                 /> Completo
             </Campo>
 
-            <Boton type="button">Cotizar</Boton>
+            <Boton type="submit">Cotizar</Boton>
         </form>
      );
 }
