385d3c3f8aab50c208867036d36d9727bd3dd842
David Robinson <david.robinson@logicstudio.net>
2020-04-29 18:00:25 -0500

    feat(cripto-app): lee los valores del select hook

--- criptomonedas/src/hooks/useMoneda.js
+++ criptomonedas/src/hooks/useMoneda.js
@@ -1,13 +1,19 @@
 import React, { Fragment, useState } from 'react';
 
-const useMoneda = () => {
-    const [state, actualizarState] = useState('');
+const useMoneda = (label, stateInicial, opciones) => {
+    const [state, actualizarState] = useState(stateInicial);
 
     const Seleccionar = () => (
         <Fragment>
-            <label>Moneda</label>
-            <select>
-                <option value="MXN">Peso Mexicano</option>
+            <label>{label}</label>
+            <select
+                onChange={e => actualizarState(e.target.value)}
+                value={state}
+            >
+                <option value="">- Seleccione -</option>
+                {opciones.map(opcion => (
+                    <option key={opcion.codigo} value={opcion.codigo}>{opcion.nombre}</option>
+                ))}
             </select>
         </Fragment>
     );
