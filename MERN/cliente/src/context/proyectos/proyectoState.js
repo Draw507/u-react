1cad4f99201731f87d08ba901d84ddf194662b68
David Robinson <david.robinson@logicstudio.net>
2020-05-04 14:33:19 -0500

    feat(mern-app): llamando proyectos con dispatch

--- MERN/cliente/src/context/proyectos/proyectoState.js
+++ MERN/cliente/src/context/proyectos/proyectoState.js
@@ -1,16 +1,18 @@
 import React, {useReducer} from 'react';
 import proyectoContext from './proyectoContext';
 import proyectoReducer from './proyectoReducer';
-import { FORMULARIO_PROYECTO } from '../../types';
+import { FORMULARIO_PROYECTO, OBTENER_PROYECTOS } from '../../types';
 
 
 const ProyectoState = props => {
+    const proyectos = [
+        {id:1, nombre: 'Tienda Virtual'},
+        {id:2, nombre: 'Intranet'},
+        {id:3, nombre: 'Diseño de sitio web'}
+    ];
+
     const initialState = {
-        proyectos: [
-            {id:1, nombre: 'Tienda Virtual'},
-            {id:2, nombre: 'Intranet'},
-            {id:3, nombre: 'Diseño de sitio web'}
-        ],
+        proyectos: [],
         formulario : false
     };
 
@@ -23,12 +25,21 @@ const ProyectoState = props => {
         })        
     };
 
+    //Obtener los proyectos
+    const obtenerProyectos = () => {
+        dispatch({
+            type: OBTENER_PROYECTOS,
+            payload: proyectos
+        })
+    };
+
     return (
         <proyectoContext.Provider
             value={{
                 proyectos: state.proyectos,
                 formulario: state.formulario,
-                mostrarFormulario
+                mostrarFormulario,
+                obtenerProyectos
             }}
         >
             {props.children}
