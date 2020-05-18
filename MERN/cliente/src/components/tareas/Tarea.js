8d14395808059a1f884fdd1d1f94e4623d322485
David Robinson <david.robinson@logicstudio.net>
2020-05-18 15:12:48 -0500

    feat(mern-app): eliminar una tarea

--- MERN/cliente/src/components/tareas/Tarea.js
+++ MERN/cliente/src/components/tareas/Tarea.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import proyectoContext from '../../context/proyectos/proyectoContext';
+import tareaContext from '../../context/tareas/tareaContext';
 
 const Tarea = ({tarea}) => {
+
+    const proyectosContext = useContext(proyectoContext);
+    const tareasContext = useContext(tareaContext);
+
+    const { proyecto } = proyectosContext;
+    const { eliminarTarea, obtenerTareas } = tareasContext;
+
+    const tareaEliminar = id => {
+        eliminarTarea(id);
+        obtenerTareas(proyecto[0].id);
+    };
+
     return ( 
         <li className="tarea sombra">
             <p>{tarea.nombre}</p>
@@ -30,6 +44,7 @@ const Tarea = ({tarea}) => {
                 <button
                     type="button"
                     className="btn btn-secundario"
+                    onClick={() => tareaEliminar(tarea.id)}
                 >Eliminar</button>
             </div>
         </li>
