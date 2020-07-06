68a9ff826123b0c0fe7dc04858c411388754d6f5
David Robinson <david.robinson@logicstudio.net>
2020-07-06 10:16:31 -0500

    feat(mern): hashear password

--- MERN/servidor/controllers/usuarioController.js
+++ MERN/servidor/controllers/usuarioController.js
@@ -1,4 +1,5 @@
 const Usuario = require('../models/Usuario');
+const bcryptjs = require('bcryptjs');
 
 exports.crearUsuario = async (req, res) => {
 
@@ -15,6 +16,10 @@ exports.crearUsuario = async (req, res) => {
 
         //crea usuario
         usuario = new Usuario(req.body);
+
+        // hashear el password
+        const salt = await bcryptjs.genSalt(10);
+        usuario.password = await bcryptjs.hash(password, salt);
 
         //guardar usuario
         await usuario.save();
