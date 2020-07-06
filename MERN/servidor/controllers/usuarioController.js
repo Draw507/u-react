e42a520bd09c620f9ea40cf68ee613a87d08d3cc
David Robinson <david.robinson@logicstudio.net>
2020-07-06 10:10:56 -0500

    feat(mern): registro de usuarios

--- MERN/servidor/controllers/usuarioController.js
+++ MERN/servidor/controllers/usuarioController.js
@@ -1,3 +1,28 @@
-exports.crearUsuario = (req, res) => {
-    console.log(req.body);
+const Usuario = require('../models/Usuario');
+
+exports.crearUsuario = async (req, res) => {
+
+    const {email, password} = req.body;
+
+    try{
+
+        //revisar que el usuario sea unico
+        let usuario = await Usuario.findOne({ email });
+
+        if(usuario){
+            return res.status(400).json({msg: 'El Usuario ya existe'});
+        }
+
+        //crea usuario
+        usuario = new Usuario(req.body);
+
+        //guardar usuario
+        await usuario.save();
+
+        //mensaje de confirmacion
+        res.json({msg:'Usuario creado correctamente'});
+    } catch(error){
+        console.log(error);
+        res.status(400).send('Hubo un error');
+    }
 }
